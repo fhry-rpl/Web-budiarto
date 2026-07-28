f8a286e75dfd1f71224a9448988ad5e94b3d6926
fhry-rpl <cloneop878@gmail.com>
2026-07-28 09:48:56 +0700

    fix: ensure bootstrap/cache directory exists on Vercel

--- public/index.php
+++ public/index.php
@@ -13,6 +13,11 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 // Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
 
+$cacheDir = __DIR__.'/../bootstrap/cache';
+if (! is_dir($cacheDir)) {
+    mkdir($cacheDir, 0755, true);
+}
+
 // Bootstrap Laravel and handle the request...
 /** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
